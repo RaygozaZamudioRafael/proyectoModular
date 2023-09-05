74dcd8a318225166671fbcb603705162808a0e63
RaygozaRafael <raygoza.zamudio.rafael@gmail.com>
2023-09-04 23:12:16 -0500

    Clase DetectorEnfermedadesActivity Implementacion parte 1

---
+++ DetectorEnfermedadesdelTomateApp/app/src/main/java/com/example/detectorenfermedadesdeltomateapp/DetectorEnfermedadesActivity.java
@@ -0,0 +1,39 @@
+package com.example.detectorenfermedadesdeltomateapp;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.app.AlertDialog;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class DetectorEnfermedadesActivity extends AppCompatActivity {
+
+    protected String mScreenName;
+
+    protected SharedPreferences prefs;
+
+    public abstract void setActivityName();
+    public abstract void onVolleyResponse(String response);
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        prefs = getSharedPreferences("DetectorEnfermedades", MODE_PRIVATE);
+        setActivityName();
+    }
+
+    public void displayAlertDialog(String title, String message){
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok,null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+
+
+    
+}
